674e6ed9173211ee375f49bda579310080005999
Noval Al-Iqbal <novalmts123@gmail.com>
2022-06-06 11:14:37 +0700

    first commit

---
+++ app/src/main/java/com/example/intenct/TampilDataActivity.kt
@@ -0,0 +1,23 @@
+package com.example.intenct
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class TampilDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tampil_data)
+
+        val tvData = findViewById<TextView>(R.id.tvData)
+
+        val nim = intent.getStringExtra("NIM")
+        val nama = intent.getStringExtra("Nama")
+
+        tvData.text = """
+            Data yang dimasukan: 
+            NIM: $nim
+            Nama: $nama
+        """.trimIndent()
+    }
+}
